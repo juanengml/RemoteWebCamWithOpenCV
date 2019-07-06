32437664fbdc2fb1106093837b28561bce14e2a4
juanengml <juanengml@gmail.com>
2019-07-06 01:59:39 -0300

    Update

--- webapp/detectface.py
+++ webapp/detectface.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 import cv2
-import sys
-import paho.mqtt.client as mqtt
+#import sys
+#import paho.mqtt.client as mqtt
 
-client = mqtt.Client()
-client.connect("192.168.100.3", 1883)
+#client = mqtt.Client()
+#client.connect("192.168.100.3", 1883)
 
 
 x=0
@@ -13,7 +13,7 @@ y=0
 cascPath = "cascades/haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascPath)
 font=cv2.FONT_HERSHEY_SIMPLEX
-video_capture = cv2.VideoCapture(0)
+video_capture = cv2.VideoCapture(1)
 
 while True:
     # CAPTURE FRAME-BY-FRAME
@@ -33,20 +33,21 @@ while True:
     # DRAW A RECTANGLE AROUND THE FACES FOUND
     if len(faces) >= 1:
      print "Face detect"
-     client.publish("home/sala/porta/01/status/"," 1 ")
+ #    client.publish("home/sala/porta/01/status/"," 1 ")
      for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 100), 1)
         
         #---To write the x,y on the middle of the rectangle.
         stringxy="+%s,%s"%(x,y) # To prepare the string with the xy values to be used with the cv2.putText function
         #In the case we want to put Xxvalue,Yyvalue we can use the following line removing #.
-        #stringaxy="X%s,Y%s"%(x,y) 
+        #fig
+        stringaxy="X%s,Y%s"%(x,y) 
         cv2.putText(frame,stringxy,(x+w/2,y+h/2),font, 1,(0,0,255),1)
         cv2.imwrite('face.jpg', frame)
     
     else:
         print "Not face "
-        client.publish("home/sala/porta/01/status/"," 0 ")
+ #       client.publish("home/sala/porta/01/status/"," 0 ")
         cv2.imwrite('face.jpg', frame)
     # DISPLAY THE RESULTING FRAME
     cv2.imshow('Video', frame)
