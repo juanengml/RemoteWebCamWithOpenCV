6395d9f7fdef5f66ca82f7a0cc7dfdcf6519bd09
juan manoel <juanengml@gmail.com>
2019-07-19 23:13:22 -0300

    Update

--- webapp/detectface.py
+++ webapp/detectface.py
@@ -10,10 +10,11 @@ import sys
 x=0
 y=0
 
-cascPath = "cascades/haarcascade_frontalface_default.xml"
+cascPath = "haarcascade_fullbody.xml"
 faceCascade = cv2.CascadeClassifier(cascPath)
 font=cv2.FONT_HERSHEY_SIMPLEX
 video_capture = cv2.VideoCapture(1)
+#video_capture = cv2.VideoCapture("movie.avi")
 
 while True:
     # CAPTURE FRAME-BY-FRAME
@@ -25,29 +26,27 @@ while True:
         gray,
         scaleFactor=1.1,
         minNeighbors=5,
-        minSize=(50, 50),
-        #when the values are smallers, the face to detect can be smaller
-  #      flags=cv2.cv.CV_HAAR_SCALE_IMAGE
-
-    )
+        minSize=(50, 50),)
     # DRAW A RECTANGLE AROUND THE FACES FOUND
     if len(faces) >= 1:
      print "Face detect"
+     cv2.putText(img = frame, text = 'Body found:' + str(len(faces)), org=(50,50), fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=1, color=(0, 0, 255))
+     #system("echo  '%s %s' > data.txt" % (len(faces_rects), 1 ))
  #    client.publish("home/sala/porta/01/status/"," 1 ")
      for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 100), 1)
-        
+
         #---To write the x,y on the middle of the rectangle.
         stringxy="+%s,%s"%(x,y) # To prepare the string with the xy values to be used with the cv2.putText function
         #In the case we want to put Xxvalue,Yyvalue we can use the following line removing #.
-        #stringaxy="X%s,Y%s"%(x,y) 
+        #stringaxy="X%s,Y%s"%(x,y)
         cv2.putText(frame,stringxy,(x+w/2,y+h/2),font, 1,(0,0,255),1)
-        cv2.imwrite('face.jpg', frame)
-    
+        cv2.imwrite('people.jpg', frame)
+
     else:
-        print "Not face "
+        print "Not body "
   #      client.publish("home/sala/porta/01/status/"," 0 ")
-        cv2.imwrite('face.jpg', frame)
+        cv2.imwrite('body.jpg', frame)
     # DISPLAY THE RESULTING FRAME
     cv2.imshow('Video', frame)
     print x,y,
